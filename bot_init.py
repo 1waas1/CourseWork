aad77b63c18d1c5bf4fd67793ccdc74290a453e6
yaroslav <gordyuk1999@gmail.com>
2019-04-13 00:06:32 +0300

    'initial'

---
+++ bot_init.py
@@ -0,0 +1,10 @@
+import telebot
+from telebot.types import Message
+
+TOKEN = "838626620:AAEfM7W3LcrxruBicAYyGb8mWFUs0I4NwuQ"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def text(message: Message):
+    bot.reply_to(message, message.text.upper())
+bot.polling()
