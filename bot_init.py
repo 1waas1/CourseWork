fdff7713139d93b2c51fe7e678655b4dc91d7fd1
yaroslav <gordyuk1999@gmail.com>
2019-04-13 19:30:17 +0300

    maybe but

--- bot_init.py
+++ bot_init.py
@@ -6,12 +6,9 @@ bot = telebot.TeleBot(TOKEN)
 
 
 
-markup = types.ReplyKeyboardMarkup()
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
 itembtna = types.KeyboardButton('a')
 itembtnv = types.KeyboardButton('v')
-itembtnc = types.KeyboardButton('c')
-itembtnd = types.KeyboardButton('d')
-itembtne = types.KeyboardButton('e')
-markup.row(itembtna, itembtnv)
-markup.row(itembtnc, itembtnd, itembtne)
+
+markup.add(itembtna, itembtnv)
 
