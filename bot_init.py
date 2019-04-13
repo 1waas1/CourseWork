d4004c3ab232f6aad5dc7cacf414e0b0fb328daa
yaroslav <gordyuk1999@gmail.com>
2019-04-13 18:44:07 +0300

    'keybord_button3'

--- bot_init.py
+++ bot_init.py
@@ -6,9 +6,12 @@ bot = telebot.TeleBot(TOKEN)
 
 
 
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('a')
+itembtnv = types.KeyboardButton('v')
+itembtnc = types.KeyboardButton('c')
+itembtnd = types.KeyboardButton('d')
+itembtne = types.KeyboardButton('e')
+markup.row(itembtna, itembtnv)
+markup.row(itembtnc, itembtnd, itembtne)
 
-keyboard = types.ReplyKeyboardMarkup(row_width=2)
-url_button = types.KeyboardButton("Перейти в гугл")
-keyboard.add(url_button)
-
-bot.polling()
