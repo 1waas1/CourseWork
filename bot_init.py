05a69364ef9530faf7ed8212c50bf844acd0dfd0
yaroslav <gordyuk1999@gmail.com>
2019-04-13 17:55:43 +0300

    'but'

--- bot_init.py
+++ bot_init.py
@@ -1,9 +1,15 @@
 import telebot
-from telebot.types import Message
+from telebot import types
 
 TOKEN = "838626620:AAEfM7W3LcrxruBicAYyGb8mWFUs0I4NwuQ"
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(content_types=["text"])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти в гугл", url="https://google.com")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
 @bot.message_handler(func=lambda message: True)
 def text(message):
     bot.reply_to(message, message.text.upper())
