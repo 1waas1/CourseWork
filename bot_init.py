550167464b93ce52e1589c4ab99e997f573c8117
yaroslav <gordyuk1999@gmail.com>
2019-04-13 18:02:16 +0300

    'keybord_button'

--- bot_init.py
+++ bot_init.py
@@ -7,9 +7,8 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=["text"])
 def default_test(message):
     keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Перейти в гугл", url="https://google.com")
+    url_button = types.KeyboardButton(text="Перейти в гугл")
     keyboard.add(url_button)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
 @bot.message_handler(func=lambda message: True)
 def text(message):
     bot.reply_to(message, message.text.upper())
