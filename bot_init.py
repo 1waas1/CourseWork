7937cd51d8328aaed43658063781adbe9351d068
yaroslav <gordyuk1999@gmail.com>
2019-04-13 18:05:41 +0300

    'but2'

--- bot_init.py
+++ bot_init.py
@@ -4,12 +4,11 @@ from telebot import types
 TOKEN = "838626620:AAEfM7W3LcrxruBicAYyGb8mWFUs0I4NwuQ"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(content_types=["text"])
-def default_test(message):
+
+@bot.message_handler(func=lambda message: True)
+def text(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.KeyboardButton(text="Перейти в гугл")
     keyboard.add(url_button)
-@bot.message_handler(func=lambda message: True)
-def text(message):
-    bot.reply_to(message, message.text.upper())
+
 bot.polling()
