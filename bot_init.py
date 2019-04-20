ba8d863071a72b429d153aefd500c8bf9b9a0c47
yaroslav <gordyuk1999@gmail.com>
2019-04-20 21:00:38 +0300

    "v 0.12"

--- bot_init.py
+++ bot_init.py
@@ -1,15 +1,22 @@
 import telebot
+import Token
 from telebot import types
 
-TOKEN = "838626620:AAEfM7W3LcrxruBicAYyGb8mWFUs0I4NwuQ"
-bot = telebot.TeleBot(TOKEN)
-
-
+bot = telebot.TeleBot(Token.TOKEN)
 
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
 a = types.KeyboardButton('Мои Товары')
 v = types.KeyboardButton('Поиск товара')
 markup.add(a, v)
+
+markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+but1 = types.KeyboardButton('Телефоны')
+but2 = types.KeyboardButton('Планшеты')
+but3 = types.KeyboardButton('Телевизоры')
+but4 = types.KeyboardButton('Ноутбуки')
+
+markup1.add(but1, but2, but3, but4)
+
 
 @bot.message_handler(commands= ["start"])
 def start_bot(message):
@@ -21,8 +28,14 @@ def start_bot(message):
 def user_product(message):
     bot.reply_to(message, "fds")
 
+@bot.message_handler(regexp="Поиск товара")
+def user_product(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Категория", reply_markup = markup1)
+
 @bot.message_handler(func=lambda message: True)
 def text(message):
     bot.reply_to(message, message.text, reply_markup = markup)
+
 
 bot.polling()
